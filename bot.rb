6a4f5c7b97847ba31d0baed2e4d6afbe89ec15d0
Mujtaba Abbas <81996938+nonofurbeeswax@users.noreply.github.com>
2021-05-19 10:47:57 +0300

    Repurposed.

--- bot.rb
+++ bot.rb
@@ -1,28 +1,72 @@
 require 'telegram/bot'
+require 'nokogiri'
+require 'httparty'
 
-token = '1642924455:AAHM54yIj88gMbpCjbkotDdoEos-mHVpo58'
-tags = Array.new
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    puts "#{message.from.username} : #{message.text}"
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "هلو, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "باي, #{message.from.first_name}")
-    when 'بوت سويلي تاك'
-      if "#{tags.join}".include?(message.from.username)
-        bot.api.send_message(chat_id: message.chat.id, text: "موجود تاك لمعرفك اذا تريد امسحة دز بوت امسح تاكي")
-      else
-        tags.push('@')
-      tags.push(message.from.username)
-      tags.push(' ')
-      bot.api.send_message(chat_id: message.chat.id, text: "تمام")
-    end 
-    when 'بوت تاك للكل'
-      if (tags.size > 0 )
-      bot.api.send_message(chat_id: message.chat.id, text: "#{tags.join}")
-      end
+  token = '1721382415:AAHCeN8oEHW7gIH3705IAXV_4lng8wWaZRY'
+  
+def ranker_error(response)
+  (response.body.nil? || response.body.empty?) || response.code != 200
+end
+
+def log_error (error)
+  Services.statstd_client.increment("learn_to_rank.errors.#{error}")
+end
+
+
+  def scraper (word)
+    url = 'https://metymology.ch'
+    unparsed_page = HTTParty.get(url)
+
+    if ranker_error(unparsed_page)
+      log_error "ranker_error"
+      return nil
     end
-  end
+
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    alphabet = parsed_page.css('ul.wpg-list-items')  #has 26 blocks in alphabetical order
+   index = word[0].upcase.codepoints.join.to_i - 65 
+   letter_block = alphabet[index].css('a')              #specify the letter block to speed up the process
+                         
+   letter_block.each do |term|
+                  if (word.downcase.eql?(term.text.downcase))
+                 found_the_word = true;
+                 term_url = term.attributes["href"].value  
+                 unparsed_term = HTTParty.get(term_url)
+                 if ranker_error(unparsed_term)
+                  log_error "ranker_error"
+                  return nil
+                end
+            
+                 parsed_term = Nokogiri::HTML(unparsed_term)
+                 description = parsed_term.css('div.entry-content.clr').text
+                 return description
+                 
+    end
+   
+   end
+  
+   return "Sorry, This word isn't in my dictionary, Care to submit it? "
+   end
+  
+
+Telegram::Bot::Client.run(token) do |bot|
+
+  bot.listen do |message|
+  
+    puts "#{message.from.username} : #{message.text}"
+   case message.text
+    when '/start'
+
+      bot.api.send_message(chat_id:message.chat.id,text:"If you encounter any error, Feel free to contact me @nonofurbeeswax")
+
+    when message.text
+if (message.text != '/start')
+
+  bot.api.send_message(chat_id:message.chat.id,text:scraper(message.text))
+
+end
+
+    end
+    end
+   
 end
